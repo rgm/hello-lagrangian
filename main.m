1ba3920a63c834d35f807b1fda2a72181f0caa45
Ryan McCuaig <rgm@ryanmccuaig.net>
2013-03-01 10:07:49 -0800

    Annotate main

--- main.m
+++ main.m
@@ -1,20 +1,16 @@
 #import <Foundation/Foundation.h>
 #import <Lagrangian/Lagrangian.h>
 
-@l3_suite("main");
+@l3_suite("main");                       // test suite declaration
 
-@l3_test("this test should fail") {
+@l3_test("this test should succeed") {   // test case
   l3_assert(YES, l3_equals(NO));
-}
-
-@l3_test("this test should succeed") {
-  l3_assert(NO, l3_equals(NO));
 }
 
 int main(int argc, const char * argv[])
 {
   @autoreleasepool {
-    l3_main(argc, argv);
+    l3_main(argc, argv);                // test runner hook
     NSLog(@"Hello, Lagrangian");
   }
   return 0;
